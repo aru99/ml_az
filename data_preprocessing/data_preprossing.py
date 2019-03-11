1fd18f59c3b95dfbe8d7af8c35d76618825ffe04
arman <mohammadarmanmavric@gmail.com>
2019-03-11 21:09:34 +0530

    featuring scaling done

--- data_preprocessing/data_preprossing.py
+++ data_preprocessing/data_preprossing.py
@@ -36,9 +36,14 @@ Y = label_encoder_Y.fit_transform(Y)
 
 #splitting the data set into the traning set and test set
 from sklearn.cross_validation import train_test_split
+
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y, test_size = 0.2, random_state = 0)
 
-
+#featuring scaling 
+from sklearn.preprocessing import StandardScaler 
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 
 
 
