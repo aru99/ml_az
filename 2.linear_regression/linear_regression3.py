ea2c8a90696054602a4621d46cd11d95e7d9acc4
arman <mohammadarmanmavric@gmail.com>
2019-03-12 13:53:17 +0530

    started working on the third example

---
+++ 2.linear_regression/linear_regression3.py
@@ -0,0 +1,29 @@
+# importing libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing datasets
+dataset = pd.read_csv('test.csv')
+X =dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 1].values
+
+# splliting the data set into traning set and test set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
