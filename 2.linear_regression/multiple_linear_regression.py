1747ff976d9758fd1cb7347c2cce8e1213f77309
arman <mohammadarmanmavric@gmail.com>
2019-03-14 05:20:32 +0530

    Fitted the multiple linear regression to the traning set : Done

--- 2.linear_regression/multiple_linear_regression.py
+++ 2.linear_regression/multiple_linear_regression.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 # importing the dataset
 dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
@@ -27,6 +28,10 @@ X = X[:, 1:]
 
 # Splitting the dataset into training set and test set
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+
+# fitting the multiple linear regression to the training set
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
 
 
 
