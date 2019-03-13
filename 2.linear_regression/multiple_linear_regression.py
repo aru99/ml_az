387967f4615c5d945c8fe37e2c94c1db858830e0
arman <mohammadarmanmavric@gmail.com>
2019-03-14 05:15:44 +0530

    Categorical data encoding : done || Dummy Variable trap : Done || Dataset splitting for traning and testing : Done

--- 2.linear_regression/multiple_linear_regression.py
+++ 2.linear_regression/multiple_linear_regression.py
@@ -8,12 +8,42 @@ Date : 13/3/19
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
 # importing the dataset
 dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 4].values
 
-#encoding categorical data
+# encoding categorical data
+# encoding the independent variable
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the dummy variable trap
+X = X[:, 1:]
+
+# Splitting the dataset into training set and test set
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
 
 
