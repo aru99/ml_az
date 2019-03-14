c61381be961ff478aab20668ba07ad7f5037ea7f
arman <mohammadarmanmavric@gmail.com>
2019-03-14 05:44:23 +0530

    added the value of X0 to the X  matrix

--- 2.linear_regression/multiple_linear_regression.py
+++ 2.linear_regression/multiple_linear_regression.py
@@ -11,6 +11,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import statsmodels.formula.api as sm
 # importing the dataset
 dataset = pd.read_csv("50_Startups.csv")
 X = dataset.iloc[:, :-1].values
@@ -27,11 +28,17 @@ X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into training set and test set
-X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
 # fitting the multiple linear regression to the training set
 regressor = LinearRegression()
 regressor.fit(X_train, Y_train)
+
+# predicting the test results
+Y_pred = regressor.predict(X_test)
+
+# building the optimal model using the Backward Elimination
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
 
 
 
