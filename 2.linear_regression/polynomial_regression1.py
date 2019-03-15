a3b2325678faef5804de1e4365a9fecd97d4d5e8
arman <mohammadarmanmavric@gmail.com>
2019-03-15 10:47:54 +0530

    Accurate visualisation : Done || Tested with different degrees : Done || used a new array X_grid as the learning curve : Done

--- 2.linear_regression/polynomial_regression1.py
+++ 2.linear_regression/polynomial_regression1.py
@@ -21,7 +21,7 @@ lin_reg = LinearRegression()
 lin_reg.fit(X, Y)
 
 # polynomial regression
-poly_reg = PolynomialFeatures(degree=2)
+poly_reg = PolynomialFeatures(degree=5)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, Y)
@@ -35,6 +35,26 @@ plt.ylabel("salary")
 plt.show()
 
 # visualising polynomial regression
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape(len(X_grid),1)
+plt.scatter(X, Y, color="red", marker=".")
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.title("truth or bluff (ploynomial regression)")
+plt.xlabel("position level")
+plt.ylabel("salary")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
