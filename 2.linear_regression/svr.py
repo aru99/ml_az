844a48503ed1c20681eb3d2996d3586569ef75c8
arman <mohammadarmanmavric@gmail.com>
2019-03-16 11:30:54 +0530

    smooth curve generation : Done

--- 2.linear_regression/svr.py
+++ 2.linear_regression/svr.py
@@ -39,7 +39,12 @@ plt.ylabel("salary")
 plt.show()
 
 
-
+# higer resolution curve
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,Y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.show()
 
 
 
