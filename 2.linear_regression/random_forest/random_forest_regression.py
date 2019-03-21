7a2059d2b628e2d0a0a377718ba73bdd371c17ed
arman <mohammadarmanmavric@gmail.com>
2019-03-21 14:13:21 +0530

    dataset inported and splitted into test and traning set : Done

---
+++ 2.linear_regression/random_forest/random_forest_regression.py
@@ -0,0 +1,29 @@
+"""
+author: Arman
+Date : 21/3/19
+"""
+# importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:2].values
+Y = dataset.iloc[:, 2].values
+
+# 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
