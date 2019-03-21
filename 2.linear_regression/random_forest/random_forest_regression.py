b412328127dc481e907cbd73da7847404c8d3c0d
arman <mohammadarmanmavric@gmail.com>
2019-03-21 14:17:53 +0530

    fitted random forest regression : Done

--- 2.linear_regression/random_forest/random_forest_regression.py
+++ 2.linear_regression/random_forest/random_forest_regression.py
@@ -12,7 +12,12 @@ dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:,1:2].values
 Y = dataset.iloc[:, 2].values
 
-# 
+# Fitting random forest regression to the dataset
+from sklearn.ensemble import RandomForestRegressor
+# n_estimators = number of trees
+regressor = RandomForestRegressor(n_estimators=10, criterion='mse', random_state=0)
+regressor.fit(X, Y)
+# pre
 
 
 
