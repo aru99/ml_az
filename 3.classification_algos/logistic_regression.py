3f725dfb0bdc3c2b86b496a46368a5f723c097f4
arman <mohammadarmanmavric@gmail.com>
2019-03-29 13:31:36 +0530

    dataset immport : Done

--- 3.classification_algos/logistic_regression.py
+++ 3.classification_algos/logistic_regression.py
@@ -4,3 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # importing the data set 
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+Y = dataset.iloc[:, 4].values
+
