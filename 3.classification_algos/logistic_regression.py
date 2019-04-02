45b0d65de9ea74f355ab1844bc42e48b66ce8599
arman <mohammadarmanmavric@gmail.com>
2019-04-02 11:18:08 +0530

    minor changes

--- 3.classification_algos/logistic_regression.py
+++ 3.classification_algos/logistic_regression.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+
 # immporting another library for traning set and test set
 from sklearn.model_selection import train_test_split
 # importing library for feature scaling
@@ -36,39 +38,6 @@ Y_pred = classifier.predict(X_test)
 # making the confusion matrix
 cm = confusion_matrix(Y_test, Y_pred)
 
-# visualising the results for traning set
-X_set, Y_Set = X_train, Y_train
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
-
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_set[Y_Set == j, 0], X_set[Y_Set == j, 1], c=ListedColormap(('white', 'yellow'))(i), label=j)
-
-plt.title('logistic regression traning set')
-plt.xlabel('Age')
-plt.ylabel('estimated salary')
-plt.legend()
-plt.show()
 
 
-# visualising the results for test set
-X_set, Y_Set = X_test, Y_test
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green')))
 
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(Y_Set)):
-    plt.scatter(X_set[Y_Set == j, 0], X_set[Y_Set == j, 1], c=ListedColormap(('white', 'yellow'))(i), label=j)
-
-plt.title('logistic regression test set')
-plt.xlabel('Age')
-plt.ylabel('estimated salary')
-plt.legend()
-plt.show()
