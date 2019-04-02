c7904a1e6e075dfd729d12dd733e945f046236e5
arman <mohammadarmanmavric@gmail.com>
2019-04-02 12:44:06 +0530

    confusion matrix : Done || visualising the results : Done

--- 3.classification_algos/k_nn/knn.py
+++ 3.classification_algos/k_nn/knn.py
@@ -12,7 +12,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 # library for feature scaling
 from sklearn.preprocessing import StandardScaler
-
+# importing library for K_nn algo
+from sklearn.neighbors import KNeighborsClassifier
+# importing library for confusion matrix
+from sklearn.metrics import confusion_matrix
 
 # importing the dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
@@ -27,4 +30,50 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
 
+# fitting the classifier to the training set
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, Y_train)
 
+# predicting the test results
+Y_pred = classifier.predict(X_test)
+
+# making the confusion matrix
+cm = confusion_matrix(Y_test, Y_pred)
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+
+X_set, Y_set = X_train, Y_train
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c=ListedColormap(('white', 'yellow'))(i), label=j)
+plt.title('K-NN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+
+X_set, Y_set = X_test, Y_test
+X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75, cmap=ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_set)):
+    plt.scatter(X_set[Y_set == j, 0], X_set[Y_set == j, 1],
+                c=ListedColormap(('white', 'yellow'))(i), label=j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
